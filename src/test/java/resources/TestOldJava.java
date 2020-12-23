a2b189fc2f3e90cedd135d491814193090b08e95
phinehasz <zhhiyp@qq.com>
2020-12-23 22:58:32 +0800

    first commit

---
+++ src/test/java/resources/TestOldJava.java
@@ -0,0 +1,14 @@
+package resources;
+
+/**
+ * @author zhhiyp
+ * @version : TestOldJava.java 2020-12-23 21:25
+ */
+public class TestOldJava {
+
+	private String abc2;
+
+	public void foo(){
+		System.out.println(111);
+	}
+}
---
+++ src/test/java/resources/TestOldJava2.java
@@ -0,0 +1,15 @@
+package resources;
+
+/**
+ * @author zhhiyp
+ * @version : TestOldJava.java 2020-12-23 21:25
+ */
+public class TestOldJava {
+
+	private String abc;
+
+
+	public void foo(){
+		System.out.println(111);
+	}
+}
